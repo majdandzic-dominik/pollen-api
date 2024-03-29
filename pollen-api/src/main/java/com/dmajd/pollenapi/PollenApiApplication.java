aeaf0ce1df1c7ab45f8e3a49cdc424269271e0e4
unknown <dominik.majdandzic@gmail.com>
2024-03-29 09:53:12 +0100

    Added base project

---
+++ pollen-api/src/main/java/com/dmajd/pollenapi/PollenApiApplication.java
@@ -0,0 +1,13 @@
+package com.dmajd.pollenapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PollenApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PollenApiApplication.class, args);
+	}
+
+}
---
+++ pollen-api/src/test/java/com/dmajd/pollenapi/PollenApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dmajd.pollenapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PollenApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
